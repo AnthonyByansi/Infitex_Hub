a3e3266af296fbd14982e98002f05ba0972ca6d4
Anthony_256 <byansianthony@gmail.com>
2023-08-05 16:23:19 +0000

    modify footer

--- src/components/layouts/general/footers/TextMainFooter.jsx
+++ src/components/layouts/general/footers/TextMainFooter.jsx
@@ -5,6 +5,15 @@ const TextMainFooter = () => {
     <div className="widget widget-text">
       <p>Welcome to the INFITX Empowerment Hub, where we believe in empowering women with the knowledge and resources to achieve financial independence and prosperity.</p>
       <p>Our mission is to provide interactive financial literacy courses, facilitate access to microfinance services, and showcase investment opportunities that align with women's financial goals and values.</p>
+      <p>Contact us at:</p>
+      <p>
+        <strong>INFITX Empowerment Hub</strong><br />
+        Plot 123, Kampala Road<br />
+        Kampala, Uganda
+      </p>
+      <p>Email: info@infitxempowermenthub.ug<br />
+         Phone: +256 789 123 456
+      </p>
     </div>
     /* <!-- /.widget-text --> */
   );

51a87ab24e9e402d2f62c97129cb5723c7b57086
Anthony_256 <byansianthony@gmail.com>
2023-08-05 16:58:23 +0000

    modify the top bar

--- src/components/layouts/general/TopBar.jsx
+++ src/components/layouts/general/TopBar.jsx
@@ -8,12 +8,12 @@ class TopBar extends Component {
                     <div className="row">
                         <div className="col-md-12">
                             <ul className="flat-infomation">
-                                <li className="phone">Call us: <Link to="+61383766284" title="phone">+61 3 8376 6284</Link></li>
-                                <li className="email">Email: <Link to="mailto:support24-7@gmail.com" title="email">support24-7@gmail.com</Link></li>
+                                <li className="phone">Call us: <Link to="+61383766284" title="phone">+256 787 686 313</Link></li>
+                                <li className="email">Email: <Link to="mailto:support24-7@gmail.com" title="email">support@infitxhub.com</Link></li>
                             </ul>
                             <div className="flat-questions">
-                                <Link to="#" title="" className="questions">Have any questions?</Link>
-                                <Link to="#" title="" className="appointment">GET AN APPOINTMENT</Link>
+                                {/* <Link to="#" title="" className="questions">Have any questions?</Link>
+                                <Link to="#" title="" className="appointment">GET AN APPOINTMENT</Link> */}
                             </div>
                             <div className="clearfix"></div>
                         </div>

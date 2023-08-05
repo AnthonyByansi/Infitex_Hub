e104302bf0c75ab01a13b2c173786a1b3ed486d1
Anthony_256 <byansianthony@gmail.com>
2023-08-05 16:20:34 +0000

    Add the footer section

--- src/components/layouts/general/footers/TextMainFooter.jsx
+++ src/components/layouts/general/footers/TextMainFooter.jsx
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TextMainFooter extends Component {
-    render() {
-        return (
-            <div className="widget widget-text">
-                <p>Lorem ipsum dolor sit consectetur adipiscing elit sed
-                    do eiusmod tempor incididunt ut labore et dolore 
-                        magna veniam aliqua.Ut enim ad minim veniam
-                </p>
-                <p>
-                    Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate 
-                </p>	
-            </div>
-            /* <!-- /.widget-text --> */
-        );
-    }
-}
+const TextMainFooter = () => {
+  return (
+    <div className="widget widget-text">
+      <p>Welcome to the INFITX Empowerment Hub, where we believe in empowering women with the knowledge and resources to achieve financial independence and prosperity.</p>
+      <p>Our mission is to provide interactive financial literacy courses, facilitate access to microfinance services, and showcase investment opportunities that align with women's financial goals and values.</p>
+    </div>
+    /* <!-- /.widget-text --> */
+  );
+};
 
 export default TextMainFooter;

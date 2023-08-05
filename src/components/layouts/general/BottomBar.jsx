9772a06a288ff5bdb267f1c9685d4a3be7fa0fe2
Anthony_256 <byansianthony@gmail.com>
2023-08-05 19:57:53 +0000

    Bottombar modification

--- src/components/layouts/general/BottomBar.jsx
+++ src/components/layouts/general/BottomBar.jsx
@@ -8,13 +8,13 @@ class BottomBar extends Component {
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="copyright">
-                                    <p>Copyright 2023 InFITX Empowerment Hub</p>
+                                    <p>Copyright 2023 InFITX . Theme by <Link to="#" title="">Themesflat</Link></p>
                                 </div>
                                 <ul className="menu-footer">
-                                    {/* <li><Link to="/" onClick={() => {window.location.href="/"}} title="">Home</Link></li>
+                                    <li><Link to="/" onClick={() => {window.location.href="/"}} title="">Home</Link></li>
                                     <li><Link to="/blog" onClick={() => {window.location.href="/blog"}} title="">My Blog</Link></li>
                                     <li><Link to="/" onClick={() => {window.location.href="/"}} title="">My Home</Link></li>
-                                    <li><Link to="#" title="">Sample Page</Link></li> */}
+                                    <li><Link to="#" title="">Sample Page</Link></li>
                                 </ul>
                                 {/* <!-- /.menu-footer --> */}
                             </div>

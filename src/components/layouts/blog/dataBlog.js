6edb2ac8980bc4ab21dd9d0ffeae0525927ba73d
Anthony_256 <byansianthony@gmail.com>
2023-08-07 08:50:17 +0000

    modify the datablog

--- src/components/layouts/blog/dataBlog.js
+++ src/components/layouts/blog/dataBlog.js
@@ -2,33 +2,33 @@ const dataBlog = [
     {
         id: 1,
         srcimg: 'images/blog/blog-post-01.png',
-        title: 'WHY DO I NEED TO USE FINANCIAL ?',
-        time: '11 Jan 2021',
-        author: 'admin',
-        category: 'Business',
-        comments: 'No comment',
-        description: 'Sed facilisis lorem in orci bibendum ullamcorper. Mauris vitae augue elementum, sodales nulla a, semper ligula. Nullam vel enim risus. Integer rhoncus hendrerit sem egestas porttitor. Integer et mi sed dolor eleifend pretium quis ut velit. Nam sit amet arcu feugiat, consequat orci at, ultrices magna'
+        title: 'The Importance of Financial Planning',
+        time: '11 Jan 2023',
+        author: 'Admin',
+        category: 'Personal Finance',
+        comments: 'No comments yet',
+        description: 'Effective financial planning is crucial for securing your future and achieving your financial goals. Whether you are looking to save for a major purchase, invest for retirement, or protect your family with insurance, sound financial planning can make a significant difference. Our expert team at Financial Solutions is dedicated to helping you navigate the complexities of personal finance and create a tailored plan to suit your unique needs and aspirations. Start your journey to financial success with us today!'
     },
     {
         id: 2,
         srcimg: 'images/blog/blog-post-02.png',
-        title: 'Why your sales forecast is off',
-        time: '11 Jan 2021',
-        author: 'admin',
-        category: 'Finanve',
-        comments: 'No comment',
-        description: 'Sed facilisis lorem in orci bibendum ullamcorper. Mauris vitae augue elementum, sodales nulla a, semper ligula. Nullam vel enim risus. Integer rhoncus hendrerit sem egestas porttitor. Integer et mi sed dolor eleifend pretium quis ut velit. Nam sit amet arcu feugiat, consequat orci at, ultrices magna'
+        title: 'Mastering Sales Forecasting for Business Growth',
+        time: '11 Jan 2023',
+        author: 'Admin',
+        category: 'Business Strategies',
+        comments: 'No comments yet',
+        description: 'Accurate sales forecasting is essential for any business to thrive. It allows you to anticipate market trends, manage inventory effectively, and make informed business decisions. At Financial Insights, we provide valuable insights and strategies to improve your sales forecasting accuracy and boost business growth. Our team of experts is ready to assist you in understanding market dynamics, identifying growth opportunities, and optimizing your sales process. Stay ahead of the competition with our sales forecasting expertise!'
     },
     {
         id: 3,
         srcimg: 'images/blog/blog-post-03.png',
-        title: '6 tips to retain your top sales talent',
-        time: '11 Jan 2021',
-        author: 'admin',
-        category: 'Insurance, Leasing',
-        comments: 'No comment',
-        description: 'Sed facilisis lorem in orci bibendum ullamcorper. Mauris vitae augue elementum, sodales nulla a, semper ligula. Nullam vel enim risus. Integer rhoncus hendrerit sem egestas porttitor. Integer et mi sed dolor eleifend pretium quis ut velit. Nam sit amet arcu feugiat, consequat orci at, ultrices magna'
+        title: 'Retaining Top Sales Talent: 6 Key Tips',
+        time: '11 Jan 2023',
+        author: 'Admin',
+        category: 'Human Resources',
+        comments: 'No comments yet',
+        description: 'Building a strong and motivated sales team is crucial for business success. However, retaining top sales talent can be a challenging task. At Talent Solutions, we understand the importance of employee retention and offer practical tips to keep your top sales performers engaged and committed. From competitive compensation packages to a positive work culture, our strategies will help you create an environment where sales professionals can thrive and contribute to your company\'s growth. Invest in your sales team\'s success with our proven retention strategies!'
     },
-    
-]
+];
+
 export default dataBlog;
